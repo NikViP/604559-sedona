7b10a2a8f98466bc604ce8faeda1815c74956edf
NikViP <p11591@mail.ru>
2018-07-05 15:31:22 +0500

    Внешний main.js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+  document.querySelector('.button-invitation')
+    .addEventListener('click', function () {
+      document.querySelector('.invitation-modal').classList.toggle('invitation-modal-show')
+    });
