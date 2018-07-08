7d2e3f66234730487a95c0c402124fcf4acb4be7
nikvip <p11591@mail.ru>
2018-07-08 13:30:57 +0500

    Выровнял текст <h3> названия отелей, При отключенном JS -- форма открыта Звездочки рейтинга переделаны Решены вопросы с переполнением контентом

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-  document.querySelector('.button-invitation')
+document.querySelector('.invitation-modal').classList.toggle('invitation-modal-show');
+
+document.querySelector('.button-invitation')
     .addEventListener('click', function () {
       document.querySelector('.invitation-modal').classList.toggle('invitation-modal-show')
     });
